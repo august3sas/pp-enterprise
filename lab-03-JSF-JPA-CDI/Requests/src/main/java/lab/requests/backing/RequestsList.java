8b95e4628962f2af36f4a4bb341b7a3ab384f56c
Jakub Brambor <111154360+august3sas@users.noreply.github.com>
2025-05-30 16:25:07 +0200

    lab03

---
+++ lab-03-JSF-JPA-CDI/Requests/src/main/java/lab/requests/backing/RequestsList.java
@@ -0,0 +1,62 @@
+package lab.requests.backing;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.component.html.HtmlDataTable;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.Size;
+import lab.requests.data.RequestRepository;
+import lab.requests.entities.Request;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RequestScoped
+@Named
+public class RequestsList {
+    @Inject
+    private RequestRepository requestRepository;
+
+    @Size(message = "{request.size}", min = 3, max = 60)
+
+    public List<Request> getAllRequests(){
+        return requestRepository.findAll();
+    }
+
+    private String newRequest;
+    private HtmlDataTable requestsDataTable;
+    public String getNewRequest() {
+        return newRequest;
+    }
+
+    public void setNewRequest(String newRequest) {
+        this.newRequest = newRequest;
+    }
+
+    public HtmlDataTable getRequestsDataTable() {
+        return requestsDataTable;
+    }
+
+    public void setRequestsDataTable(HtmlDataTable requestsDataTable) {
+        this.requestsDataTable = requestsDataTable;
+    }
+
+    @Transactional
+    public String addRequest()
+    {
+        Request request = new Request();
+        request.setRequestDate(LocalDate.now());
+        request.setRequestText(newRequest);
+        requestRepository.create(request);
+        setNewRequest("");
+        return "requestsList.xhtml?faces-redirect=true";
+    }
+
+    @Transactional
+    public String deleteRequest() {
+        Request req = (Request) getRequestsDataTable().getRowData();
+        requestRepository.remove(req);
+        return "requestsList.xhtml?faces-redirect=true";
+    }
+}
